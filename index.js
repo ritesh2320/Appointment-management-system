58e94bbdac73676c60641b95c328479c93ddb44f
Ritesh <riteshselar4539@gmail.com>
2026-02-06 10:30:55 +0530

    cors origin expanded

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const path = require("path");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+    ],
     credentials: true,
-  }),
+  })
 );
+
 
 app.use(express.json());
 
